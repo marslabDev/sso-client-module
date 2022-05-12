f0c190bf8b2244f25405a98084d1b426110990ea
Jason <jason@marslab.com.my>
2022-05-12 10:33:51 +0800

    v0.0.1

---
+++ Entities/Role.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Modules\SSOClient\Entities;
+
+use \DateTimeInterface;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Role extends Model
+{
+    use SoftDeletes;
+    use HasFactory;
+
+    public $table = 'roles';
+    protected $connection = 'sso_db';
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $fillable = [
+        'title',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class);
+    }
+
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
+}
