21eecebe07afcf268804fd9cc5630b25d08ed429
marslabDev <104558949+marslabDev@users.noreply.github.com>
2022-06-16 15:25:37 +0800

    Update passwordEdit.blade.php

--- Resources/views/partials/passwordEdit.blade.php
+++ Resources/views/partials/passwordEdit.blade.php
@@ -1,7 +1,11 @@
 @if(file_exists(module_path('SsoClient', 'Http/Controllers/Auth/ChangePasswordController.php')))
 @can('profile_password_edit')
 <li class="c-sidebar-nav-item">
-    @include('views.partials.passwordEditBody')
+<a class="c-sidebar-nav-link href="{{ route('profile.password.edit') }}
+    {{ request()->is('profile/password') || request()->is('profile/password/*') ? 'c-active' : '' }}"
+">
+    <i class="fa-fw fas fa-key c-sidebar-nav-icon"></i>{{ trans('global.change_password') }}
+</a>
 </li>
 @endcan
 @endif
