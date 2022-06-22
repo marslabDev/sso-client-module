8ec3d4a943db18fcf557fe0d187d43b916212104
marslabDev <104558949+marslabDev@users.noreply.github.com>
2022-06-22 17:05:36 +0800

    Update passwordEdit.blade.php

--- Resources/views/partials/passwordEdit.blade.php
+++ Resources/views/partials/passwordEdit.blade.php
@@ -2,7 +2,7 @@
 @can('profile_password_edit')
 <li class="c-sidebar-nav-item">
 <a class="c-sidebar-nav-link" href="{{ route('profile.password.edit') }}"
-    {{ request()->is('profile/password') || request()->is('profile/password/*') ? 'c-active' : '' }}"
+    {{ request()->is('profile/password') || request()->is('profile/password/*') ? 'c-active' : '' }}
 >
     <i class="fa-fw fas fa-key c-sidebar-nav-icon"></i>{{ trans('global.change_password') }}
 </a>
