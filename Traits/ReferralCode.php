53f965e3f6e05f4aea3ef30e9072881daad0e253
marslabDev <104558949+marslabDev@users.noreply.github.com>
2022-08-09 11:11:04 +0800

    Reverting Change

--- Traits/ReferralCode.php
+++ Traits/ReferralCode.php
@@ -9,9 +9,9 @@ trait ReferralCode
 {
     public static function bootReferralCode()
     {
-        static::creating(function (Model $model) {
+        static::created(function (Model $model) {
             $model->referral_code = self::generateReferralCode($model->id);
-//             $model->save();
+            $model->save();
         });
     }
 
